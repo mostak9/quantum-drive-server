b139ace7659cd6b1652cc64db32238ec8113d19c
mostak9 <mostak.ahm263@gmail.com>
2023-10-22 19:33:28 +0600

    get a single cart data to find a product exist in cart or not

--- index.js
+++ index.js
@@ -63,7 +63,12 @@ async function run() {
       res.send(result);
     })
     // get a single cart product
-    
+    app.get('/cart/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id:  id};
+      const result = await cartCollection.findOne(query);
+      res.send(result);
+    })
 
 
     app.put('/update/:id', async(req, res)  => {
